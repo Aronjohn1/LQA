fc26025822f50515a493cc24da9e3eb0e2ea29eb
Aron John <204282932+Aronjohn1@users.noreply.github.com>
2026-06-18 23:54:16 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,13 @@ const requestRoutes = require("./routes/request");
 const app = express();
 const PORT = process.env.PORT || 1000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://lqa-ten.vercel.app/"
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
